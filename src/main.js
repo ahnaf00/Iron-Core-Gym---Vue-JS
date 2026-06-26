470cdd82d9492d23cb633073d035afe392615bf3
Ahnaf Anan <aanana0171@gmail.com>
2026-06-27 00:58:46 +0600

    Pinia configured

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import router from './router/router'
 import App from './App.vue'
 
 const app = createApp(App)
+const pinia  = createPinia()
+
+app.use(pinia)
 app.use(router)
 // createApp(App).mount('#app')
 app.mount('#app')
